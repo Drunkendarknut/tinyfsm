9be2a01ddeba430f05d2cf410c527c38682fe743
Håkon Stormo <haakon.stormo@gmail.com>
2017-03-18 18:24:47 +0100

    In the name of optimization ...
    no more dictionary lookups and string concatenations in Call method

--- StateMachine.cs
+++ StateMachine.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
 public class StateMachine<T> : MonoBehaviour {
 
 	private static string separator = "_";
-	private static string[] methodTypes = new string[] {
-		"Enter", "Exit", "Update", "FixedUpdate"
-	};
 
+	private enum Function { Enter, Exit, Update, FixedUpdate };
 	private MonoBehaviour component;
-	private Dictionary<string, MethodInfo> methodLookUp = new Dictionary<string, MethodInfo> {};
+	private MethodInfo[,] methodLookUp;
 	private T currentState;
 
 	public T CurrentState {
@@ -21,45 +17,42 @@ public class StateMachine<T> : MonoBehaviour {
 
 	public void Initialize (MonoBehaviour comp, T initState) {
 		component = comp;
-		// Reflect methods
 		string[] stateNames = Enum.GetNames(typeof(T));
+		string[] functionNames = Enum.GetNames(typeof(Function));
+		methodLookUp = new MethodInfo[stateNames.Length, functionNames.Length];
+		// Reflect methods
 		for (int i = 0; i < stateNames.Length; i++) {
-			for (int j = 0; j < methodTypes.Length; j++) {
-				string methodName = stateNames[i] + separator + methodTypes[j];
-				MethodInfo method = component.GetType().GetMethod(
+			for (int j = 0; j < functionNames.Length; j++) {
+				string methodName = stateNames[i] + separator + functionNames[j];
+				methodLookUp[i,j] = component.GetType().GetMethod(
 					methodName,
 					BindingFlags.NonPublic | BindingFlags.Public |
 					BindingFlags.Instance | BindingFlags.DeclaredOnly
 				);
-				if (method != null) {
-					methodLookUp.Add(methodName, method);
-				}
 			}
 		}
 		currentState = initState;
-		Call(currentState, "Enter");
+		Call(currentState, Function.Enter);
 	}
 
 	public void ChangeState (T targetState) {
 		if (!targetState.Equals(currentState)) {
-			Call(currentState, "Exit");
+			Call(currentState, Function.Exit);
 			currentState = targetState;
-			Call(currentState, "Enter");
+			Call(currentState, Function.Enter);
 		}
 	}
 
 	void Update () {
-		Call(currentState, "Update");
+		Call(currentState, Function.Update);
 	}
 
 	void FixedUpdate () {
-		Call(currentState, "FixedUpdate");
+		Call(currentState, Function.FixedUpdate);
 	}
 
-	void Call (T state, string function) {
-		string methodName = Enum.GetName(typeof(T), state) + separator + function;
-		MethodInfo method;
-		if (methodLookUp.TryGetValue(methodName, out method))
-			method.Invoke(component, null);
+	void Call (T state, Function function) {
+		MethodInfo method = methodLookUp[Convert.ToInt32(state), (int)function];
+		if (method != null) method.Invoke(component, null);
 	}
 }
